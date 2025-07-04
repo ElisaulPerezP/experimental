7e59ccae106558146091196fe669a1b891addc78
ElisaulPerezP <elisaul.perez@hotmail.com>
2025-07-03 20:54:44 -0500

    actualizacion para solucionar primer inconveniente de testeos

---
+++ Simple_Unit_Test/test/test_desktop/test_native_main.cpp
@@ -0,0 +1,17 @@
+#include <unity.h>
+#include "add.h"
+
+void test_addition() {
+    TEST_ASSERT_EQUAL(5, add(2, 3));
+    TEST_ASSERT_EQUAL(-1, add(2, -3));
+}
+
+void setUp() {}
+
+void tearDown() {}
+
+int main() {
+    UNITY_BEGIN();
+    RUN_TEST(test_addition);
+    return UNITY_END();
+}
